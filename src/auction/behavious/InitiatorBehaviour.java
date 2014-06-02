f2c9a376dfb676cf4c1fea32b863052b65da347e
Miao Sun <miaosun88@gmail.com>
2014-06-02 23:37:02 +0100

    CFP

--- src/auction/behavious/InitiatorBehaviour.java
+++ src/auction/behavious/InitiatorBehaviour.java
@@ -1,5 +1,8 @@
 package auction.behavious;
 
+import java.util.ArrayList;
+
+import jade.core.AID;
 import jade.core.Agent;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
@@ -10,36 +13,38 @@ import jade.lang.acl.ACLMessage;
 @SuppressWarnings("serial")
 public class InitiatorBehaviour extends BaseBehaviour{
 	
-	public final int CFP = 1;
-	public final int 	DEFINE_BID		= 2;
+	public final int INFORM = 1;
+	public final int 	CFP		= 2;
 	public final int	SEND_PROPOSAL	= 3;
 	public final int	GET_REPLY		= 4;
+	
+	private ArrayList<AID>	participants = new ArrayList<>();
 	
 	int n= 0;
 	
 	public InitiatorBehaviour()
 	{
-		this.state = CFP;
-		
-		System.out.println("what about here!");
+		this.state = INFORM;
+
 	}
 	
 	
 	@Override
 	public void action() {
-		// TODO Auto-generated method stub
-		System.out.println("Chegou!");
+
 		switch(state)
 		{
+		case INFORM:
+			informAuction();
+			break;
 		case CFP:
 			callForProposal();
-			System.out.println("Here!");
 			break;
 		
 		}
 	}
 
-	public void callForProposal() {
+	public void informAuction() {
 		
 		DFAgentDescription template = new DFAgentDescription();
         ServiceDescription sd1 = new ServiceDescription();
@@ -52,13 +57,22 @@ public class InitiatorBehaviour extends BaseBehaviour{
            // envia mensagem "pong" inicial a todos os agentes "ping"
            ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
            for(int i=0; i<result.length; ++i)
-              msg.addReceiver(result[i].getName());
+           {
+        	   AID receiver = result[i].getName();   
+        	   msg.addReceiver(receiver);
+        	   participants.add(receiver);
+           }
            msg.setContent("English Auctin is gonna start!");
            this.myAgent.send(msg);
            
         } catch(FIPAException e) { e.printStackTrace(); }
         
-        n++;
+        state = CFP;
+	}
+	
+	public void callForProposal() {
+		System.out.println("Call For Proposal!");
+
 	}
 
 	@Override
